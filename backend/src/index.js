0c52894d415420355a269841d40e9309ac519384
kshitijkhowal <kshitij.durgesh@gmail.com>
2024-12-15 01:19:08 +0530

    zustand and  axios working

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,17 +4,22 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 dotenv.config();
 const app=express();
 app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+    
+}))
 
 
 const port=process.env.PORT;
 
 
-app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 
