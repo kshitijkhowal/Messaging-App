65bcf870db0d7b8bb8d77627fac454a0eb6f4d5a
kshitijkhowal <kshitij.durgesh@gmail.com>
2024-12-15 17:00:33 +0530

    navbar, login and signup pages are working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,44 @@
-import React, { useEffect } from 'react';
-import Navbar from './components/Navbar';
-import { Routes,Route, Navigate } from 'react-router-dom';
-import HomePage from "./pages/HomePage";
+import React, { useEffect } from 'react'
+import {Routes,Route, Navigate} from "react-router-dom";
+import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
+import Navbar from './components/Navbar';
 import { useAuthStore } from './store/useAuthStore';
 import {Loader} from "lucide-react";
 
 
 const App = () => {
 
-  const {authUser,checkAuth,isCheckingAuth}=useAuthStore();
+  const {authUser,checkAuth,isCheckingAuth}=useAuthStore();   // state and hook
 
   useEffect(()=>{
     checkAuth();
+  }, [checkAuth]);
 
-  },[authUser]);
+  console.log({authUser});
 
-  console.log({authUser})
-
-  if(isCheckingAuth && !authUser) return(
+  if(isCheckingAuth && !authUser) return (
     <div className="flex items-center justify-center h-screen">
-      <Loader className='size-10 animate-spin'/>
+     <Loader className="size-10 animate-spin"/>
     </div>
-  ) 
-  
+  )
+
   return (
     <div>
       <Navbar/>
-      
+
       <Routes>
-        <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
-        <Route path='/signup' element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
-        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
-        <Route path='/settings' element={<SettingsPage/>}/>
-        <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login" /> }  />
+        <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/" />}  />
+        <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/" /> }  />
+        <Route path="/settings" element={<SettingsPage/>}  />
+        <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login" /> }  />
       </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
